9c5b0669e22a7a1115aa58c82d37f35fa0ceb2f1
MrSent <mrsentientone@gmail.com>
2023-01-25 21:36:54 +0000

    add conquestbuy

---
+++ ConquestBuy/map.lua
@@ -0,0 +1,6 @@
+return {
+	-- CP Items
+	[16776] = {Name="scythe",  Cost=4000, Option=32800, Index=0},
+	[16712] = {Name="axe",     Cost=4000, Option=32800, Index=0},
+	[16844] = {Name="Halberd", Cost=4000, Option=32800, Index=0},
+}
