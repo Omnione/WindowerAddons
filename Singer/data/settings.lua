511f87708d7f58551ccfb2fb72ba89703e47f3de
MrSent <mrsentientone@gmail.com>
2023-03-03 14:52:04 +0000

    Added custom singer

---
+++ Singer/data/settings.lua
@@ -0,0 +1,16 @@
+return {
+    ["dummy"] = L{"Knight's Minne",},
+    ["debuffs"] = L{},
+    ["playlist"] = T{
+        ["basic"] = L{},
+    },
+    ["buffs"] = T{
+        ["haste"] = L{},
+        ["refresh"] = L{},
+        ["aurorastorm"] = L{},
+        ["firestorm"] = L{},
+    },
+    ["song"] = T{
+    },
+    ["songs"] = L{},
+}
